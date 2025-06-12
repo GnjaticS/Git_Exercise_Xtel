665e27aadec281b52cb2248c348c3150954cd657
Stefan.Gnjatic <stefan.gnjatic.ext@xtel-group.com>
2025-06-12 09:56:50 +0200

    Simple Python Code Added

---
+++ PyCode.py
@@ -0,0 +1,10 @@
+x = 10
+y = 5
+sum = x + y
+difference = x - y
+product = x * y
+quotient = x / y
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
